bd009c40d3564b3f0782fb4705ab276da662b884
gemm123 <gemaakbar213@gmail.com>
2023-02-27 18:45:20 +0700

    update custom message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { connect, sendMsg } from "./api";
 import Header from './components/Header/Header';
 import ChatHistory from "./components/ChatHistory";
+import ChatInput from "./components/ChatInput";
 
 class App extends Component {
   constructor(props) {
@@ -22,19 +23,21 @@ class App extends Component {
     })
   }
 
-  send() {
-    console.log("hello");
-    sendMsg("hello");
+  send(event) {
+    if(event.keyCode === 13) {
+      sendMsg(event.target.value);
+      event.target.value = "";
+    }
   }
 
   render() {
     return (
       <div className="App">
-      <Header />
-      <ChatHistory chatHistory={this.state.chatHistory} />
-        <button onClick={this.send}>Hit</button>
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <ChatInput send={this.send}/>
       </div>
-    )
+    );
   }
 }
 
